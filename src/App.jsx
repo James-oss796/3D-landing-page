0e6aa6cf595ea046469206665d0d7f17c2739255
James-oss796 <jamesbriandungu@gmail.com>
2025-10-25 13:47:03 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Head } from "./Components/Head"
+import NavBar from "./Components/NavBar"
+import Test from "./Components/Test"
+import React from "react"
+const App = () => {
+  return (
+
+   <main>
+    <NavBar />
+   </main>
+  
+ 
+  )
+}
+
+export default App
